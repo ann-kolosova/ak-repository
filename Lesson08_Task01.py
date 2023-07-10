681352e52122d8e2816bbec80e3ecbe2efe5f760
Anna Kol <annakol@Annas-MacBook-Air.local>
2023-07-10 11:06:28 +0300

    Fix for Lesson08_p2

--- Lesson08_Task01.py
+++ Lesson08_Task01.py
@@ -17,23 +17,20 @@ l1 = [-7, 1, 2, 0, 3, 12, -24, 5, -1, 6]
 
 copy_l1 = l1[:]
 
-# sort the list in descending order by absolute value
-sorted_list = sorted(copy_l1, key=abs, reverse=True)
-
-# check if the length of the list is less than 3
-if len(sorted_list) < 3:
+# check if the length of the list contains at least 3 val
+if len(copy_l1) < 3:
     print('At least 3 values should be in the input')
 else:
-    max_mult = sorted_list[0] * sorted_list[1] * sorted_list[2]
-    max_nums = (sorted_list[0], sorted_list[1], sorted_list[2])
+    max_mult = copy_l1[0] * copy_l1[1] * copy_l1[2]
+    max_nums = (copy_l1[0], copy_l1[1], copy_l1[2])
 
     # find the maximum multiplication among three values
-    for i in range(len(sorted_list) - 2):
-        for j in range(i + 1, len(sorted_list) - 1):
-            for k in range(j + 1, len(sorted_list)):
-                mult = sorted_list[i] * sorted_list[j] * sorted_list[k]
+    for i in range(len(copy_l1) - 2):
+        for j in range(i + 1, len(copy_l1) - 1):
+            for k in range(j + 1, len(copy_l1)):
+                mult = copy_l1[i] * copy_l1[j] * copy_l1[k]
                 if mult > max_mult:
                     max_mult = mult
-                    max_nums = (sorted_list[i], sorted_list[j], sorted_list[k])
+                    max_nums = (copy_l1[i], copy_l1[j], copy_l1[k])
 
     print(f'Max value = "{max_mult}". Nums are: {max_nums}')
